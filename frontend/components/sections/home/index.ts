33906541f4f7bbd332c4a028ad1b5bfce47e96a8
KEERTHIAANAND <keerthiaanand77@gmail.com>
2026-03-05 16:38:39 +0530

    Add ScoutingReachCard and VerifiedBadge components with layout and styling

---
+++ frontend/components/sections/home/index.ts
@@ -0,0 +1,4 @@
+// Home Section Components barrel export
+export { default as HeroSection } from "./HeroSection";
+export { default as ScoutingReachCard } from "./ScoutingReachCard";
+export { default as VerifiedBadge } from "./VerifiedBadge";
