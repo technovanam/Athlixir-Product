e6d0d12014313f7f9abaf04cecb751a49006a6d5
Sasi <sasikiran.tt3011@gmail.com>
2026-03-09 01:11:22 +0530

    chore: update UI primitive components

--- frontend/components/ui/StarBorder.tsx
+++ frontend/components/ui/StarBorder.tsx
@@ -43,7 +43,7 @@ const StarBorder = <T extends React.ElementType = 'button'>({
           animationDuration: speed
         }}
       ></div>
-      <div className="relative z-1 bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
+      <div className="relative z-1 bg-surface-2 border border-surface-3 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
         {children}
       </div>
     </Component>
